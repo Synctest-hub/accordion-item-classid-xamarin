8758b795202b7cc2a652d9474d5712152870c888
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-22 17:07:48 +0530

    XAMARIN-34556 - Sample and screenshot added.

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Helper/Behavior.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.XForms.Accordion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+    public class Behavior : Behavior<SfAccordion>
+    {
+        SfAccordion Accordion;
+        protected override void OnAttachedTo(SfAccordion bindable)
+        {
+            Accordion = bindable;
+            Accordion.Expanded += Bindable_Expanded;
+            Accordion.Collapsed += Bindable_Collapsed;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void Bindable_Expanded(object sender, ExpandedAndCollapsedEventArgs e)
+        {
+            var index = e.Index;
+            var item = Accordion.Items[index];
+            if (item.ClassId =="Item1")
+            {
+                App.Current.MainPage.DisplayAlert("Informtion", "Accordion Item1 Expanded", "Ok");
+            }
+            else if (item.ClassId == "Item2")
+            {
+                App.Current.MainPage.DisplayAlert("Informtion", "Accordion Item2 Expanded", "Ok");
+            }
+            else if (item.ClassId == "Item3")
+            {
+                App.Current.MainPage.DisplayAlert("Informtion", "Accordion Item3 Expanded", "Ok");
+            }
+            else if (item.ClassId == "Item4")
+            {
+                App.Current.MainPage.DisplayAlert("Informtion", "Accordion Item4 Expanded", "Ok");
+            }
+        }
+
+        private void Bindable_Collapsed(object sender, ExpandedAndCollapsedEventArgs e)
+        {
+        }
+
+        protected override void OnDetachingFrom(SfAccordion bindable)
+        {
+            Accordion.Expanded -= Bindable_Expanded;
+            Accordion.Collapsed -= Bindable_Collapsed;
+            Accordion = null;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Model/ItemInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AccordionXamarin
+{
+    public class ItemInfo : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _description;
+        private string _classID;
+
+        public string ClassID
+        {
+            get => _classID;
+            set
+            {
+                _classID = value;
+                OnPropertyChanged("ClassID");
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string Property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Property));
+        }
+    }
+}
